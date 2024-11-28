ac4347388fecf83984567580ab1094b25e269a1f
VNK4 <ivan.ignatov2504@gmail.com>
2024-11-28 21:54:35 +0200

    Fix isPrime

--- week05/solutions/task04.cpp
+++ week05/solutions/task04.cpp
@@ -42,7 +42,7 @@ int fact(int n) {
 }
 
 bool isPrime(int n) {
-    for(int i = 0; i < n / 2; ++i) {
+    for(int i = 2; i < n / 2; ++i) {
         if(n % i == 0) {
             return false;
         }
